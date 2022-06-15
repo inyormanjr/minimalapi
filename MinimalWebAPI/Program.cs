a9eb81decc90eb2c6ad41a72aec60f44baab65d5
inyormanjr <ronilojunioryap@gmail.com>
2022-06-16 07:49:42 +1000

    initial

---
+++ MinimalWebAPI/Program.cs
@@ -0,0 +1,31 @@
+﻿using MinimalWebAPI.Endpoints;
+using MinimalWebAPI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseHttpsRedirection();
+
+app.MapAccountEndPoint();
+
+app.Run();
