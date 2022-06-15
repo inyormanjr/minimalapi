c62aec087efb6997e7fbc3699fddf2f7698bbe75
inyormanjr <ronilojunioryap@gmail.com>
2022-06-16 08:08:10 +1000

    updated account endpoint

--- MinimalWebAPI/Endpoints/AccountEndpoint.cs
+++ MinimalWebAPI/Endpoints/AccountEndpoint.cs
@@ -9,18 +9,23 @@ namespace MinimalWebAPI.Endpoints
 	{
 		public static void MapAccountEndPoint(this WebApplication app)
         {
+			//registration endpoint
 			app.MapPost("/account/register", async (AccountServices accountService, CreateUserDTO createUser) => {
 				var result = await accountService.Register(createUser);
 				return Results.Ok(result);
 			}).Produces<UserDTO>();
 
+
+			//login endpoint
 			app.MapPost("/account/login", async (AccountServices accountService, LoginUserDTO loginUserDTO) =>
 			{
 				var user = await accountService.Login(loginUserDTO);
+				if (user == null) return Results.BadRequest("Invalid user credentials");
 				var token = JWTTokenHelper.CreateToken(user, app.Configuration);
 				LoginResponseUserDTO responseUserDTO = new() { Email = user.Email, Token = token };
 				return Results.Ok(responseUserDTO);
-			}).Produces<LoginResponseUserDTO>().ProducesProblem(500, "Invalid User Credentials");
+
+			}).Produces<LoginResponseUserDTO>();
         }
 	}
 }
