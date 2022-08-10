da52a8329b7d1ac63b65d0c9d81f9e5de2728fd4
inyormanjr <ronilojunioryap@gmail.com>
2022-08-11 00:21:59 +1000

    commit latest changes

---
+++ BLL/Services/Users/UserService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using AutoMapper;
+using BLL.DTO.Users;
+using DAL.IRepositories;
+using Domain.DomainAgg.UserAgg;
+
+namespace BLL.Services.Users
+{
+	public class UserService
+	{
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+		{
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+
+        public  async Task<List<UserDTO>> GetMany(int skip, int take)
+        {
+            var result = await this.userRepository.GetList(skip, take);
+            var mapped = this.mapper.Map<List<UserDTO>>(result);
+            return mapped;
+        }
+
+        public async Task<UserDTO> GetById(int id)
+        {
+            var result = await this.userRepository.GetById(id);
+            var mapped = this.mapper.Map<UserDTO>(result);
+            return mapped;
+        }
+
+        public async Task<UserDTO?> Update(int id, UserDTO userDTO)
+        {
+            var checkedUser = await this.userRepository.GetById(id);
+            if (checkedUser == null) return null;
+             var merged  = mapper.Map(userDTO, checkedUser);
+            var result = await this.userRepository.Update(id, merged);
+            var remapped = mapper.Map<UserDTO>(result);
+            return remapped;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+             var result = await this.userRepository.Delete(id);
+            return result;
+        }
+	}
+}
+
---
+++ MinimalWebAPI/Endpoints/UserEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BLL.DTO.Users;
+using BLL.Services.Users;
+
+namespace MinimalWebAPI.WebApp.Endpoints
+{
+	public static class UserEndpoint
+	{
+		public static void MapUserEndpoint(this WebApplication app)
+        {
+			app.MapGet("/users", async (UserService userService, int skip, int take) => {
+				var result = await userService.GetMany(skip, take);
+				return Results.Ok(result);
+			}).Produces<List<UserDTO>>();
+
+			app.MapGet("/users/{id}", async (UserService userService, int id) =>
+            {
+				var result = await userService.GetById(id);
+				if (result == null) return Results.NotFound("No User found");
+				return Results.Ok(result);
+            }).Produces<UserDTO>();
+
+			app.MapPut("/users/{id}", async (UserService userService,int id, UserDTO userDTO) =>
+			{
+				UserDTO? result = await userService.Update(id, userDTO);
+				Results.Ok(result);
+			}).Produces<UserDTO>();
+
+			app.MapDelete("/users/{id}", async (UserService userService, int id) =>
+            {
+				var result = await userService.Delete(id);
+				return Results.Ok(result);
+            }).Produces<bool>();
+        }
+	}
+}
+
